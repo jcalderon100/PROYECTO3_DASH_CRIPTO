e81b18f1c0121bbbca1f65fb7189a9bba1369f62
Julio C <112023055+jcalderon100@users.noreply.github.com>
2022-11-25 19:58:54 -0700

    integracion de chart.js

--- funciones.js
+++ funciones.js
@@ -12,9 +12,23 @@
         console.log(dato)
         render.innerHTML += `
         <div class='card'>
-            <p>${dato.name}</p>
-            <p>${dato.priceUsd}</p>
+            Moneda <p>${dato.name}</p> Precio USD <p>${dato.priceUsd}</p>
+
         </div>
         `
     })
 }
+
+// const API_URL = "https://api.coincap.io/v2/assets";
+
+// const HTMLResponse = document.querySelector("#app");
+
+
+// fetch(`${API_URL}/data`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//         const template = data.map((data) => `<li>${data.id } correo: ${data.priceUsd}</li>`)
+//         HTMLResponse.innerHTML = `<ul>${template}</ul>`;
+
+//     });
+
